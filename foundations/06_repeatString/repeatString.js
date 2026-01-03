ecb112063c5a460ce46cfa2593536349f2243b09
bhavya-choudhary-githost <214997610+bhavya-choudhary-githost@users.noreply.github.com>
2026-01-03 20:06:03 +0530

    completed exercises 6-7of js foundations

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str,num) {
+    let result = []
 
+    if(num < 0){return "ERROR";}
+
+    else{  
+        for(let i = 0; i < num; i++){
+        result.push(str)}}
+
+    return result.join('')
+ 
 };
 
 // Do not edit below this line
